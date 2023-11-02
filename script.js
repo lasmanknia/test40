6446d7357a0cde420cd9e2b515052c6fdd72efeb
Salman Anik <86368691+lasmanknia@users.noreply.github.com>
2023-11-02 22:19:19 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,83 @@
+
+//type writer
+
+var span = document.querySelector(".typewriter span");
+var textArr = span.getAttribute("data-text").split(", "); 
+var maxTextIndex = textArr.length; 
+
+var sPerChar = 0.15; 
+var sBetweenWord = 1.5;
+var textIndex = 0; 
+
+typing(textIndex, textArr[textIndex]); 
+
+function typing(textIndex, text) {
+    var charIndex = 0; 
+    var maxCharIndex = text.length - 1; 
+    
+    var typeInterval = setInterval(function () {
+        span.innerHTML += text[charIndex]; 
+        if (charIndex == maxCharIndex) {
+            clearInterval(typeInterval);
+            setTimeout(function() { deleting(textIndex, text) }, sBetweenWord * 1000); 
+            
+        } else {
+            charIndex += 1; 
+        }
+    }, sPerChar * 1000); 
+}
+
+function deleting(textIndex, text) {
+    var minCharIndex = 0; 
+    var charIndex = text.length - 1; 
+
+    var typeInterval = setInterval(function () {
+        span.innerHTML = text.substr(0, charIndex); 
+        if (charIndex == minCharIndex) {
+            clearInterval(typeInterval);
+            textIndex + 1 == maxTextIndex ? textIndex = 0 : textIndex += 1; 
+            setTimeout(function() { typing(textIndex, textArr[textIndex]) }, sBetweenWord * 1000); 
+        } else {
+            charIndex -= 1; 
+        }
+    }, sPerChar * 1000); 
+}
+
+//progressbar
+
+const counters = document.querySelectorAll(".counter");
+
+counters.forEach((counter) => {
+  counter.innerText = "0";
+  const updateCounter = () => {
+    const target = +counter.getAttribute("data-target");
+    const count = +counter.innerText;
+    const increment = target / 300;
+    if (count < target) {
+      counter.innerText = `${Math.ceil(count + increment)}`;
+      setTimeout(updateCounter, 1);
+    } else counter.innerText = target;
+  };
+  updateCounter();
+});
+
+//background
+
+  const changeColorButton = document.getElementById("change-bg-color");
+    const colorInput = document.getElementById("color-input");
+    const root = document.documentElement; // Reference to the :root element
+
+    // Retrieve the color from local storage (if set previously)
+    const storedColor = localStorage.getItem("backgroundColor");
+    if (storedColor) {
+      root.style.setProperty('--background-color', storedColor);
+      colorInput.value = storedColor;
+    }
+
+    changeColorButton.addEventListener("click", () => {
+      const selectedColor = colorInput.value;
+      root.style.setProperty('--background-color', selectedColor);
+
+      // Store the selected color in local storage
+      localStorage.setItem("backgroundColor", selectedColor);
+    });
